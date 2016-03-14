4e11b23c57e9f7191d16c8131e059ec356c7d2e2
Yiyi Zhai <yiyizhai0723@gmail.com>
2016-03-14 16:59:07 -0400

    Set up a nodejs server

---
+++ server.js
@@ -0,0 +1,22 @@
+//library we're gonna use --> express
+//Install express lib to package.json
+var express = require('express');
+
+//get an express object
+var app = express();
+
+//Without this part, get a weird err: cannot Get/: cuz the url is not defined
+//Whenever get a url '/', do the function
+//req: you're resquesting something
+//res: server responses with something
+app.get('/name',function(req, res){
+	//response with some data
+	var name = "Yiyi";
+	res.json("My name is " + name);
+})
+
+//run the server
+app.listen(8000, function(err){
+	if(err) throw err;
+	console.log("Server runs on port 8000");
+});
